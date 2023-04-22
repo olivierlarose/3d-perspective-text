e5a8ba16a0501fda9b3435e3efcf6769b382f4fa
Olivier Larose <olivierlarose1@gmail.com>
2023-04-22 18:05:02 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,13 @@ export default function Home() {
 
   const plane = useRef(null);
   const maxRotate = 45;
-  useEffect(() => {
-    window.addEventListener("mousemove", manageMouseMove)
-    return () => {
-      window.removeEventListener("mousemove", manageMouseMove);
-    }
-  }, [])
 
   const manageMouseMove = (e) => {
     const x = e.clientX / window.innerWidth
     const y = e.clientY / window.innerHeight
     const perspective = window.innerWidth * 4;
     const rotateX = maxRotate * x - maxRotate / 2; 
-    const rotateY = maxRotate * y - maxRotate / 2;
+    const rotateY = (maxRotate * y - maxRotate / 2) * - 1;
     plane.current.style.transform = `perspective(${perspective}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg)`
   }
 
